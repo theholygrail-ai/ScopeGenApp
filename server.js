08e38fd6a997437bc0f7c1ed6635d3cd25bee1d3
Erwin <fxholygrailsa@gmail.com>
2025-07-31 20:32:51 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -1005,8 +1005,8 @@ async function sowOrchestrator(brdText, brandContext) {
 }
 // --- Configure Middleware & Routes ---
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(fileUpload());
 app.use('/slides', slideRoutes);
 app.get('/brandcontext', (req, res) => {
