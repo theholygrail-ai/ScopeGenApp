0459cd4d9ad761e10bb10ece15ab06d44631e306
theholygrail-ai <fxholygrailsa@gmail.com>
2025-07-31 18:55:37 +0200

    Increase body parser limits

--- server.js
+++ server.js
@@ -983,8 +983,8 @@ async function sowOrchestrator(brdText) {
 }
 // --- Configure Middleware & Routes ---
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(fileUpload());
 app.use('/images/headshots', express.static(tempDir));
 
